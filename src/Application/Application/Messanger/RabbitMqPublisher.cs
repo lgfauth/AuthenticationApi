c628f9e0349402aaa16b6535c1f5963a6c668b27
Luan Fauth <luan.fauth@outlook.com>
2025-03-10 21:38:16 -0300

    Correção de configuração de fila do publisher e adição de fila de retry nas variaveis

--- src/Application/Application/Messanger/RabbitMqPublisher.cs
+++ src/Application/Application/Messanger/RabbitMqPublisher.cs
@@ -12,19 +12,19 @@ namespace Application.Messanger
     public class RabbitMqPublisher : IRabbitMqPublisher
     {
         private readonly ConnectionFactory _factory;
-        private readonly string _queueName;
+        private readonly EnvirolmentVariables _variables;
 
         public RabbitMqPublisher(EnvirolmentVariables envorolmentVariables)
         {
+            _variables = envorolmentVariables;
+
             _factory = new ConnectionFactory()
             {
-                HostName = envorolmentVariables.RABBITMQCONFIGURATION_HOSTNAME,
-                UserName = envorolmentVariables.RABBITMQCONFIGURATION_USERNAME,
-                Password = envorolmentVariables.RABBITMQCONFIGURATION_PASSWORD,
-                VirtualHost = envorolmentVariables.RABBITMQCONFIGURATION_VIRTUALHOST
+                HostName = _variables.RABBITMQCONFIGURATION_HOSTNAME,
+                UserName = _variables.RABBITMQCONFIGURATION_USERNAME,
+                Password = _variables.RABBITMQCONFIGURATION_PASSWORD,
+                VirtualHost = _variables.RABBITMQCONFIGURATION_VIRTUALHOST
             };
-
-            _queueName = envorolmentVariables.RABBITMQCONFIGURATION_QUEUENAME;
         }
 
         public async Task PublishUserRegistrationOnQueueAsync(UserQueueRegister message)
@@ -32,12 +32,22 @@ namespace Application.Messanger
             using var connection = await _factory.CreateConnectionAsync();
             using var channel = await connection.CreateChannelAsync();
 
-            _ = await channel.QueueDeclareAsync(queue: _queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            _ = await channel.QueueDeclareAsync(
+                    queue: _variables.RABBITMQCONFIGURATION_QUEUENAME,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: new Dictionary<string, object>
+                    {
+                        { "x-dead-letter-exchange", "" },
+                        { "x-dead-letter-routing-key", _variables.RABBITMQCONFIGURATION_RETRY_QUEUENAME }
+                    }!
+                );
 
             var json = JsonSerializer.Serialize(message);
             ReadOnlyMemory<byte> body = Encoding.UTF8.GetBytes(json);
 
-            await channel.BasicPublishAsync(exchange: "", routingKey: _queueName, body: body);
+            await channel.BasicPublishAsync(exchange: "", routingKey: _variables.RABBITMQCONFIGURATION_QUEUENAME, body: body);
         }
     }
 }
